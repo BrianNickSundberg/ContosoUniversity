a8b6de17da5a74d604f988b00745c0021ee8fea5
BrianNickSundberg <BrianNickSundberg@users.noreply.github.com>
2019-07-15 07:48:18 -0700

    Initial commit.
    Initial commit. The base line .

---
+++ Migrations/20190711174643_addDepartmentName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class addDepartmentName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentName",
+                table: "Course",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepartmentName",
+                table: "Course");
+        }
+    }
+}
